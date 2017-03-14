5f19caf3d297f66283ae3a2fa7068e11ec58a396
Tihomir Minev <tihomir.minev.sci@gmail.com>
2017-03-14 11:39:01 +0200

    register chek up
    register chek

--- index.php
+++ index.php
@@ -11,14 +11,14 @@ session_start();
 </head>
 <body>
 <form action="" method="post">
-	username<input type="text" name="user_name">
-	password<input type="password" name="password">
+	username<input type="text" name="user_name" value="" placeholder="user">
+	password<input type="password" name="password" value="">
 	<!-- <input type="hidden" name="count" value="<?php echo $count?> "> -->
 	<input type="submit" name="submit" value="login">		
 	</form>
 
 <?php 
-
+include('db_connection.php');
 if (!empty($_POST['submit'])) {
 	$_SESSION['user_name'] = $_POST['user_name'];
 	echo "Hello, ".$_SESSION['user_name'];
@@ -29,32 +29,23 @@ if (!empty($_POST['submit'])) {
 $_SESSION['count'] = rand(10,20);
 $_SESSION['money'] = rand(50,100);
 
-include('db_connection.php');
-
-
-
 $read_q = "SELECT * FROM `users` WHERE `date_deleted` IS NULL";
-
 $result = mysqli_query($conn, $read_q);
-//var_dump($result);
+
 $i=0;
 if (mysqli_num_rows($result) > 0) {
-	
 	while ($row = mysqli_fetch_assoc($result)) {
-		
-		
 			$array[$i] = $row['user_name'];
-		$i++;
+			$i++;		
+			}
+}
 
-			
-}
-}
-if (in_array( $_POST['user_name'], $array)) {
+if (in_array($_SESSION['user_name'], $array)) {
 	
-	header('Location: game.php');
+	//header('Location: game.php');
 }
 else{
-	echo "<br>molq registrirai se ". $_POST['user_name'] . " takuv";
+	echo "<br>molq registrirai se ". $_SESSION['user_name'] . " takuv";
 }
 
 
