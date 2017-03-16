6e067927565b63bbd239ae48d85dab5a2c97e0c1
galissima <galissima@abv.bg>
2017-03-16 21:34:55 +0200

    We have just one style/css

--- includes/footer.php
+++ includes/footer.php
@@ -1,17 +1,6 @@
 </body>
 <footer>
 <div class="footer">All rights reserved.&copy;2017г. <br> Designed by Галя Петкова&Тихомир Минев</div>
-<style type="text/css">
 
-	.footer{
-		color: black;
-		text-align: right;
-		width: 28%;
-		height: 20px;
-		/*background-color: red;*/
-		position: absolute;
-		top: 600px;
-		right: 5px;	}
-</style>
 </footer>
 </html>
