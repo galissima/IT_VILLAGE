c404b1e30ff41381fe77eecaa9fb6b1b7764ea50
galissima <galissima@abv.bg>
2017-03-18 18:50:13 +0200

    deleted files

--- game_over.php
+++ game_over.php
@@ -8,11 +8,11 @@ include('includes/header.php');
 
 <p id="game">GAME</p>
 <p id="over">OVER</p>
-<p id="score">SCORE: <?php echo $_SESSION['money'] ?> </p>
-<p id="score">SCORE: <?php echo $_SESSION['result'] ?> </p>
+ <p id="score">SCORE: <?php echo $_SESSION['money'] ?> </p> 
+<p id="score_text">SCORE: <?php echo $_SESSION['result'] ?> </p> -->
 </div>
-<a href="scores.php" class="newgame">NewGame</a>
-<a href="index.php" class="logout">LogOut</a>
+<a href="games_score.php" class="newgame" id="hover1">NewGame</a>
+<a href="index.php" class="logout" id="hover2">LogOut</a>
 
 <?php
 include('includes/footer.php');
--- games_score.php
+++ games_score.php
@@ -26,3 +26,45 @@ if (mysqli_num_rows($result) > 0) {
 	}
 	echo "</table>";
 }
+
+
+include('db_connection.php');
+//READ ROWS FROM TABLE GAMES
+$read_query = "SELECT * FROM `users` WHERE `date_deleted` IS NULL"; 
+$result = mysqli_query($conn, $read_query);
+
+
+if (mysqli_num_rows($result) > 0) {
+	echo "<table border=1>";
+	echo "<tr><th>USER_ID</th><th>USER_NAME</th><th>USER_PASSWORD</th><th>USER_EMAILL</th><th>USER_CURRENT_SCORE</th>";
+	while ($row = mysqli_fetch_assoc($result)) {
+		echo "<tr>";
+		
+			echo "<td>" . $row['user_id'] . "</td>";
+			echo "<td>" . $row['user_name'] . "</td>";
+			echo "<td>" .$row['user_password'] . "</td>";
+			echo "<td>" .$row['user_email'] . "</td>";
+			echo "<td>" .$row['user_current_score'] . "</td>";
+
+	
+		echo "<tr>";
+
+	}
+	echo "</table>";
+
+}
+session_start();
+if (!empty ($_SESSION['money'])) {
+	$_SESSION['user_result']= $_SESSION['money'] + $_SESSION['user_current_score'];
+	echo 'Result of ' . $_SESSION['user_name'] . ' is '. $_SESSION['user_result'];
+}
+
+// $substance_id	= $_GET['substance_id'];
+// 	$date = date('Y-m-d');
+	$update_query = "SELECT * FROM `users` ORDER BY `user_current_score` DESC";
+	$result = mysqli_query($conn, $update_query);
+	// if ($result) {
+	// 	return header('Location: games_score.php');  
+	// } else {
+	// echo "Error: " . $update_query . " - " . mysqli_error($conn);
+	// }
